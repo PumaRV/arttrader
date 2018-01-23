7dca3f825c7f3a69bbfaca576001a0bec941f620
RicardoGeek <ricardo@ricardogeek.com>
2018-01-22 22:40:14 -0600

    Adds endpoint to track ticks

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const express = require('express');
+const parser = require('body-parser');
+const sanitize = require('express-sanitize-escape');
+const fxdata = require('./lib/fxdata');
+
 const app = express();
 
+app.use(parser.urlencoded({
+    extended: true
+}));
+app.use(sanitize.middleware());
 app.use(express.static('./frontend/build'));
 
+app.post('/ticks/add', (request, respose) => {
+
+    //at this point body params had been sanitized.
+    const bid = request.body.bid;
+    const ask = request.body.ask;
+    const time = request.body.time;
+    const pair = request.body.pair;
+    const ema20 = request.body.ema20;
+
+    const result = fxdata.saveTick(bid, ask, time, pair, ema20);
+    respose.send(result.status);
+});
+
 app.listen(80, function() {
-    console.log('I hate frontend frameworks');
 });
